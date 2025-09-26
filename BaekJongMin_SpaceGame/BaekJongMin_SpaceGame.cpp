dfd83405562f398ac94b258e6d016d71d3e5104d
jackbongmin <rrrgk12@gmail.com>
2025-09-26 17:39:42 +0900

    완성 마무리

--- BaekJongMin_SpaceGame/BaekJongMin_SpaceGame.cpp
+++ BaekJongMin_SpaceGame/BaekJongMin_SpaceGame.cpp
@@ -1,5 +1,11 @@
-﻿// BaekJongMin_SpaceGame.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <Windows.h>
+#include "Meet.h"
+#include "Player.h"
+#include "IIIntro.h"
+#include "GameManager.h"
+#include <random>
+
 /*                         
                                                          ███╗   ██╗███████╗██╗  ██╗██████╗  █████╗  ██████╗███████╗    ██╗   ██╗███████╗
                                                          ████╗  ██║██╔════╝ ██╗██╗ ██╔══██╗██╔══██╗██╔════╝██╔════╝    ██║   ██║██╔════╝
@@ -8,20 +14,9 @@
                                                          ██║ ╚████║███████╗██║  ██║██║     ██║  ██║╚██████╗███████╗     ╚████╔╝ ███████║
                                                          ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝      ╚═══╝  ╚══════╝
 */
-
-#include <iostream>
-#include <Windows.h>
-#include "Meet.h"
-#include "Player.h"
-#include "IIIntro.h"
-#include "GameManager.h"
-#include <random>
-
 int main()
 {
     srand(time(0));
-    //Player player;
-    //player.PlayerStat();
     //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
     IIIntro introstart;
     introstart.Intro_1();
